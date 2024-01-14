f47af2877a7d96a16fd14f429146c517b947009d
egabszi <egabor43@gmail.com>
2024-01-14 16:03:21 +0100

    reload command added

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
+    category: 'utility',
+    cooldown: 5,
     data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-    name: Events.InteraactionCreate,
+    name: Events.InteractionCreate,
     async execute(interaction) {
         if (!interaction.isChatInputCommand()) return;
 
